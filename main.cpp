75604b351ee0e0bfc00182d2ace547a2b69cf619
egoridze74 <evkorneenko@edu.hse.ru>
2024-03-04 14:23:21 +0700

    Well done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <list>
 #include <algorithm>
+#include <fstream>
+#include <iterator>
+#include <deque>
 
 bool divisible_by_5(int n) {
     return n % 5 == 0;
@@ -12,7 +15,6 @@ void task1() {
     unsigned int amount;
     int current;
 
-    std::cout << "TASK-1:"<< std::endl;
     std::cout << "Insert amount of your numbers:" << std::endl;
     std::cin >> amount;
     std::cout << "Insert your numbers:" << std::endl;
@@ -30,14 +32,12 @@ void task1() {
         std::cout << "Not found" << std::endl;
     else
         std::cout << *result << std::endl;
-    std::cout << std::endl;
 }
 
 void task2() {
     std::vector<int> v = {13, 1337, 52, 21, 74};
     std::list<int> l;
 
-    std::cout << "TASK-2:" << std::endl;
     std::cout << "Original vector: ";
     for (auto it = v.begin(); it != v.end(); ++it) {
         std::cout << *it << " ";
@@ -62,16 +62,85 @@ void task2() {
 
     // Использование адаптера front_inserter для добавления элементов в начало
     std::fill_n(std::front_inserter(l), 3, -2);
-    std::cout << "List with new elements in the beginnig: ";
+    std::cout << "List with new elements in the beginning: ";
     for (auto it = l.begin(); it != l.end(); ++it) {
         std::cout << *it << " ";
     }
     std::cout << std::endl;
 }
 
+void task3() {
+    //Запись в файл
+    std::vector<int> v_out = {1, -2, 3, -4, 5, -6};
+    std::cout << "Our vector: ";
+    for (auto it = v_out.begin(); it != v_out.end(); ++it)
+        std:: cout << *it <<  " ";
+    std::cout << std::endl;
+    std::cout << "Copy our vector to \"file.txt\"" << std::endl;
+    std::ofstream file1("..\\file.txt");
+    std::copy(v_out.begin(), v_out.end(), std::ostream_iterator<int>(file1, " "));
+    file1.close();
+
+    // Чтение из файла
+    std::vector<int> v_in;
+    std::ifstream file2("..\\file.txt");
+    std::copy(std::istream_iterator<int>(file2), std::istream_iterator<int>(), std::back_inserter(v_in));
+    file2.close();
+    std::cout << "Vector from \"file.txt\": ";
+    for (auto it = v_in.begin(); it != v_in.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
+
+void task4() {
+    std::vector<int> v = {-1, 2, -3, 4, -5, 6};
+    std::cout << "Our vector: ";
+    for (auto it = v.begin(); it != v.end(); ++it)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+    std::deque<int> d;
+    std::copy(v.begin(), v.end(), std::back_inserter(d));
+    std::cout << "Deque with elements from vector: ";
+    for (auto it = d.begin(); it != d.end(); ++it)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+}
+
+void task5() {
+    int n, temp;
+    std::list<int> l;
+    std::cout << "Input amount of your numbers:" << std::endl;
+    std::cin >> n;
+    std::cout << "Input your numbers:" << std::endl;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> temp;
+        l.push_back(temp);
+    }
+    std::cout << "Reverse order of elements from list: ";
+    for (auto it = l.rbegin(); it != l.rend(); ++it)
+        std::cout << *it << " ";
+}
 
 int main() {
-    //task1();
+    std::cout << "Task 1:" << std::endl;
+    task1();
+    std::cout << "-----------------" << std::endl;
+
+    std::cout << "Task 2:" << std::endl;
     task2();
+    std::cout << "-----------------" << std::endl;
+
+    std::cout << "Task 3:" << std::endl;
+    task3();
+    std::cout << "-----------------" << std::endl;
+
+    std::cout << "Task 4:" << std::endl;
+    task4();
+    std::cout << "-----------------" << std::endl;
+
+    std::cout << "Task 5:" << std::endl;
+    task5();
+    std::cout << "-----------------" << std::endl;
     return 0;
 }
